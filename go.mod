b6a44c33012b36ccc4c60834447fc10d16554ad9
Lyra <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-09-13 11:29:23 +0200

    parsing defaults values

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lyra-poing-serre/meteo
 
 go 1.25.0
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("an error occured when loading .env file %s", err.Error())
+	}
+
+	apiToken := os.Getenv("API_TOKEN")
+	fmt.Printf("Hello this your token: %s\n", apiToken)
+
+	defaultCity := strings.Split(os.Getenv("DEFAULT_CITY"), ",")
+	if defaultCity[0] != "" {
+		fmt.Printf("Il y a %d ville(s) par défault\n", len(defaultCity))
+		for i, city := range defaultCity {
+			fmt.Printf("%d. %s\n", i, city)
+		}
+	} else {
+		fmt.Println("Warning: empty default value.")
+	}
 }
